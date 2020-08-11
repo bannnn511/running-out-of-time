182480cc956b3a1284365fef2410c6e126997f66
An <habinhan1999@gmail.com>
2020-08-11 19:48:52 +0700

    remove all

---
+++ Assets/RoundPlatformer/Scripts/Player_Movement.cs
@@ -0,0 +1,179 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(SpriteRenderer))]
+public class Player_Movement : MonoBehaviour{
+
+
+	[Tooltip("This is the gameobject were the player is attracted to. If nothing, the player will fly, you can change that gameobject on Runtime")]
+	public GameObject CenterOfGravity;
+	public float GravityForce;
+	
+	public float PlayerSpeed;
+	public float MaxSpeed;
+	public float JumpSpeed;
+
+	[Tooltip("For Double Jump or more. Set to 1 for a single jump")]
+	public int NumberOfJump;
+	private int JumpCount;
+	private bool IsGrounded;
+	private float distToGround;
+	private Collider2D collider;
+	public LayerMask GroundedMask;
+	
+	private Rigidbody2D RB2B;
+	
+
+	private SpriteRenderer PlayerSpriteRenderer;
+	private Animator anim;
+	private float AngularSpeedLimitation;
+
+
+
+	void Start() {
+		RB2B = GetComponent<Rigidbody2D>();
+		PlayerSpriteRenderer = GetComponent<SpriteRenderer>();
+		anim = GetComponent<Animator>();
+
+		collider = GetComponent<Collider2D>();
+		distToGround = collider.bounds.extents.y;
+		JumpCount = NumberOfJump;
+	}
+
+
+
+	void Update() {
+		On_PlayerMovement();
+		On_PlayerJump();
+		MirrorAnimationPlayer();
+		ResetNumberOfJump();
+		CheckIfPlayerGrounded();
+		GravityDrag();
+
+		Debug.DrawRay(this.transform.position, -transform.up, Color.green);
+	}
+
+
+
+
+	//This function calculate the speed limitation of the player depending of how far he is from the center of Gravity.
+	//This will prevent the player from flying if he goes too fast, too close from the center of gravity 
+	private float CalculateAngularSpeedLimitation(){
+
+		if(CenterOfGravity != null){
+			float speedLimitation;
+			float distance;
+
+			distance = Vector3.Distance(transform.position, CenterOfGravity.transform.position);
+			distance = distance/10;
+
+			speedLimitation = Mathf.Lerp(0.5F, 2F, distance);
+			speedLimitation = speedLimitation/5;
+
+			return speedLimitation;
+			
+		}
+		else{ return 1; } //If the player don't have gravity center, no speed limitation is set.
+	}
+
+
+	private void GravityDrag(){
+		if(CenterOfGravity != null){
+			RB2B.AddForce((CenterOfGravity.transform.position - transform.position) * GravityForce);
+			Vector3 dif = CenterOfGravity.transform.position - transform.position;
+			float RotationZ = Mathf.Atan2(dif.y , dif.x) * Mathf.Rad2Deg;
+			transform.rotation = Quaternion.Euler(0.0F, 0.0F, RotationZ + 90);
+		}
+	}
+
+
+
+
+
+	private void On_PlayerMovement(){
+		if(Input.GetAxis("Horizontal") != 0){
+			Vector2 localvelocity;
+			localvelocity = transform.InverseTransformDirection(RB2B.velocity);
+			localvelocity.x = Input.GetAxis("Horizontal") * Time.deltaTime * PlayerSpeed * 100 * CalculateAngularSpeedLimitation();
+			RB2B.velocity = transform.TransformDirection(localvelocity);
+
+			anim.SetBool("PlayerMoving", true);
+		}
+		else { //Slow down the player when no pressure on the Horizontal Axis (For more responcive controls).
+			
+			Vector2 localvelocity;
+			localvelocity = transform.InverseTransformDirection(RB2B.velocity);
+			localvelocity.x = localvelocity.x * 0.5F;
+			RB2B.velocity = transform.TransformDirection(localvelocity);
+
+			anim.SetBool("PlayerMoving", false);
+		}
+	}
+
+	private void On_PlayerJump(){
+		if(Input.GetButtonDown("Jump")){
+			if(JumpCount != 0){
+				Vector2 localvelocity;
+				localvelocity = transform.InverseTransformDirection(RB2B.velocity);
+				localvelocity.y = 0;
+				RB2B.velocity = transform.TransformDirection(localvelocity);
+				JumpCount --;
+				RB2B.AddRelativeForce(new Vector2(0,1) * JumpSpeed * 10, ForceMode2D.Impulse);
+			}
+		}
+	}
+
+
+	private void CheckIfPlayerGrounded(){
+		
+		if (isGrounded()) {
+			IsGrounded = true;
+			anim.SetBool("PlayerJumping", false);
+		}
+		else {
+			IsGrounded = false;
+			anim.SetBool("PlayerJumping", true);
+		}
+	}
+
+
+	private bool isGrounded(){
+		if(Physics2D.Raycast(transform.position, -transform.up, 1F, GroundedMask, -Mathf.Infinity, Mathf.Infinity)){
+			return true;
+		}
+		return false;
+	}
+
+
+
+	//Below this point, The script is doing normal stuff (like animation)//
+
+
+	private void MirrorAnimationPlayer(){
+		Vector2 localVelocity = transform.InverseTransformDirection(RB2B.velocity);
+
+		if(localVelocity.x > 0.5F){
+			if(PlayerSpriteRenderer.flipX == true){
+				PlayerSpriteRenderer.flipX = false;
+			}
+		}
+		else if (localVelocity.x < -0.5){
+			if(PlayerSpriteRenderer.flipX == false){
+				PlayerSpriteRenderer.flipX = true;
+			}
+		}
+	}
+	
+
+
+	private void ResetNumberOfJump(){
+		if(JumpCount < NumberOfJump){
+			if(IsGrounded){
+				JumpCount = NumberOfJump;
+			}
+		}
+	}
+}
---
+++ Assets/RoundPlatformer/Scripts/RoundPlatformer_Component.cs
@@ -0,0 +1,156 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(LineRenderer))]
+[RequireComponent(typeof(EdgeCollider2D))]
+public class RoundPlatformer_Component : MonoBehaviour {
+
+	/// <summary>
+	/// Control the Position of the platform. From 1 to 100. 1 = closer from the center, 100 = far from the center.
+	/// </summary>
+	[Range(1, 100)]
+	[Tooltip("Position of the platform from the center")]
+	public float Position;
+
+	/// <summary>
+	/// Control the Rotation of the platform. In degree.
+	/// </summary>
+	[Range(-360, 360)]
+	[Tooltip("Rotation of the platform")]
+	public float Rotation;
+
+
+	/// <summary>
+	/// The width of the platform. From 1 to 360. In Degree. 1 = Small platform. 360 = a complete circle.
+	/// </summary>
+	[Range(1, 365)]
+	[Tooltip("Width of the platform")]
+	public float Width;
+
+
+	/// <summary>
+	/// The height of the platform. from 0.1f to 10.
+	/// </summary>
+	//[Tooltip("Height of the platform")]
+	//[Range(0.1f, 10)]
+	//public float Height;
+
+
+	[Tooltip("Control the Z position of the visual render of the platform. Tweak this value if you want to have your platform in front or behind another element.")]
+	[Range(-10, 10)]
+	public float Z_Position;
+
+
+	/// <summary>
+	/// Number of segments. Used for tilable texture.
+	/// </summary>
+	[Space] [Space]
+	[Range(1,100)]
+	[Tooltip("")]
+	public int Segments;
+
+	/// <summary>
+	/// If true, the script will automaticly calculate the number of segments depending to the width and position of the platform. So the texture will tile correctly.
+	/// </summary>
+	[Tooltip("If true, the script will automaticly calculate the number of segments so the texture will tile correctly")]
+	public bool AutoSegments = true;
+
+
+
+	private LineRenderer Linerender;
+	private EdgeCollider2D EdgeCol2D;
+
+
+
+	private void OnEnable() {
+		Initialize();
+	}
+
+
+
+	private void Initialize(){
+		Linerender = GetComponent<LineRenderer>();
+		EdgeCol2D = GetComponent<EdgeCollider2D>();
+
+		Linerender.positionCount = Segments + 1;
+
+		CreatePoints(Width, Rotation);
+	}
+
+
+
+	//Update the Platform graphics in EditorTime//
+	public void OnValidate() {
+
+		if(Linerender == null){Linerender = GetComponent<LineRenderer>();}
+		if(EdgeCol2D == null){EdgeCol2D = GetComponent<EdgeCollider2D>();}
+		if(Width > 365){ Width = 365;}
+
+		if(Linerender != null){
+			if(AutoSegments == true){
+				RespectTextureRatio();
+			}
+
+			Linerender.positionCount = Segments + 1;
+			CreatePoints(Width, Rotation);
+			EdgeCollider_Generate(Width, Rotation);
+		}
+	}
+
+
+
+	//RespectTextureRatio will Automaticly calculate the number of segments needed so the texture will not stretch//
+	//Called only if the boolean "AutoSegment" is used. 
+	private const float AngleToSegments = 2.91F; //Don't touch this unless the AutoSegments fail to correctly calculate the number of segments for a correct tile ratio//
+	private const float RadiusToSegments = 6F; //Don't touch this either.
+
+	public void RespectTextureRatio(){
+		Segments = Mathf.RoundToInt(Width / AngleToSegments);
+		Segments = Mathf.RoundToInt(((Width / RadiusToSegments)/10) * Position);
+	}
+
+
+
+	//Create the visual of the platform
+	private void CreatePoints(float InputAngle, float Rotation){
+		float X;
+		float Y;
+		float angle = (-(InputAngle / 2)) + Rotation;
+
+		for (int i = 0; i < (Segments + 1); i++){
+			X = Mathf.Sin (Mathf.Deg2Rad * angle) * Position;
+			Y = Mathf.Cos (Mathf.Deg2Rad * angle) * Position;
+
+			Linerender.SetPosition(i, new Vector3(X, Y, Z_Position)); //The Position of the LineRenderer Point. 
+
+			angle += (InputAngle / Segments);
+		}
+	}
+
+
+
+	//Create the collider shape of the platform
+	private Vector2[] LineCoordPos;
+
+	public void EdgeCollider_Generate(float InputAngle, float rotation){
+		LineCoordPos = new Vector2[(Linerender.positionCount)];
+
+		float X;
+		float Y;
+		float angle = (-(InputAngle / 2 )) + rotation;
+
+		for (int i = 0; i < (Linerender.positionCount); i++){ //For every position of the line; Generate one point for the Edge Collider 2D Shape.
+			X = Mathf.Sin(Mathf.Deg2Rad * angle) * Position;
+			Y = Mathf.Cos(Mathf.Deg2Rad * angle) * Position;
+
+			LineCoordPos[i] = new Vector2(X, Y);
+			angle += (InputAngle/Segments);
+		}
+
+		if(EdgeCol2D != null){
+			EdgeCol2D.points = LineCoordPos;
+		}
+	}
+}
