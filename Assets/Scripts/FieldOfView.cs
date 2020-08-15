607b1c3afe0a26d1addb571171e2ab78b2cc1ce6
An Ha <32453570+bannnn511@users.noreply.github.com>
2020-08-15 17:06:53 +0700

    push cho hiu heo

--- Assets/Scripts/FieldOfView.cs
+++ Assets/Scripts/FieldOfView.cs
@@ -4,14 +4,24 @@ using UnityEngine;
 
 public class FieldOfView : MonoBehaviour
 {
+	[SerializeField] LayerMask layerMask;
+	Mesh mesh;
+	Vector3 origin;
+	float startingAngle;
+
+	float fov = 90f;
+
+
 	// Start is called before the first frame update
 	void Start()
 	{
-		Mesh mesh = new Mesh();
+		mesh = new Mesh();
 		GetComponent<MeshFilter>().mesh = mesh;
-
-		float fov = 90f;
-		Vector3 origin = Vector3.zero;
+		origin = Vector3.zero;
+	}
+	// Update is called once per frame
+	void Update()
+	{
 		int rayCount = 5;
 		float angle = 0f;
 		float angleIncrease = fov / rayCount;
@@ -19,7 +29,7 @@ public class FieldOfView : MonoBehaviour
 
 		Vector3[] vertices = new Vector3[rayCount + 1 + 1];
 		Vector2[] uv = new Vector2[vertices.Length];
-		int[] triangles = new int[rayCount];
+		int[] triangles = new int[rayCount * 3];
 
 		vertices[0] = origin;
 
@@ -27,7 +37,16 @@ public class FieldOfView : MonoBehaviour
 		int triangleIndex = 0;
 		for (int i = 0; i <= rayCount; i++)
 		{
-			Vector3 vertex = origin + GetVectorFromAngle(angle) * viewDistance;
+			Vector3 vertex;
+			RaycastHit2D raycastHit2D = Physics2D.Raycast(origin, GetVectorFromAngle(angle), viewDistance, layerMask);
+			if (raycastHit2D.collider == null)
+			{
+				vertex = origin + GetVectorFromAngle(angle) * viewDistance;
+			}
+			else
+			{
+				vertex = raycastHit2D.point;
+			}
 			vertices[vertexIndex] = vertex;
 
 			if (i > 0)
@@ -49,17 +68,30 @@ public class FieldOfView : MonoBehaviour
 		mesh.triangles = triangles;
 	}
 
-	// Update is called once per frame
-	void Update()
+	public void SetOrigin(Vector3 origin)
 	{
-
+		this.origin = origin;
 	}
 
-	static Vector3 GetVectorFromAngle(float angle)
+	public void SetDirection(Vector3 direction)
+	{
+		startingAngle = GetAngleFromVectorFloat(direction) - fov / 2f;
+	}
+	Vector3 GetVectorFromAngle(float angle)
 	{
 		// angle 0 -> 360
 		float angleRad = angle * (Mathf.PI / 180f);
 		return new Vector3(Mathf.Cos(angleRad), Mathf.Sin(angleRad));
 	}
 
+	float GetAngleFromVectorFloat(Vector3 dir)
+	{
+		dir = dir.normalized;
+		float n = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+		if (n < 0)
+		{
+			n += 360;
+		}
+		return n;
+	}
 }
