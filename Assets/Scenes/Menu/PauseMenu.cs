47f2e8cc5c8265753467df9fbd0ec230f29a9df2
hieusskymtps <hieuminhnguyen95@gmail.com>
2020-08-18 11:54:26 +0700

    add menus

---
+++ Assets/Scenes/Menu/PauseMenu.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isPausing =false;
+    public GameObject PauseMenuUI;
+    void Update()
+    {
+        if (Input.GetKeyDown("p"))
+        {
+            if (isPausing)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    public void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPausing = false;
+    }
+    public void Reset()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Resume();
+    }
+    public void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPausing = true;
+    }
+    public void Menu()
+    {
+        SceneManager.LoadScene(0);
+        Resume();
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
