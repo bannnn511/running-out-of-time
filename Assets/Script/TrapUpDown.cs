4a17af1401c1d3e7aec23b9be7ccfc2704f483b4
Van <51709517+ptv1811@users.noreply.github.com>
2020-08-20 18:21:38 +0700

    finish lv1 map

---
+++ Assets/Script/TrapUpDown.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapUpDown : MonoBehaviour
+{
+    private Vector3 velocity;
+    private void FixedUpdate()
+    {
+        transform.position += (velocity * Time.deltaTime);
+    }
+}
