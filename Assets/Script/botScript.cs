dccf6a8455c07a1e85652791040e494c95e5356e
An <habinhan1999@gmail.com>
2020-08-22 11:03:31 +0700

    add bot mirror movement

--- Assets/Script/botScript.cs
+++ Assets/Script/botScript.cs
@@ -3,135 +3,143 @@ using Pathfinding;
 
 public class botScript : MonoBehaviour
 {
-    public float direction = 1;
-    public float speed = 1.5f;
-    public GameObject CenterOfGravity;
-    public float GravityForce;
-    public bool moveRight = false;
-    public float chaseDistance = 4.5f;
-    public float repeat = 2f;
-    public Transform collisionPoint;
-    public float collisionRange = 0.5f;
-    public LayerMask enemyLayer;
-    Animator enemyAnimator;
+  public float direction = 1;
+  public float speed = 1.5f;
+  public GameObject CenterOfGravity;
+  public float GravityForce;
+  public float chaseDistance = 4.5f;
+  Animator enemyAnimator;
+  private SpriteRenderer spriteRenderer;
 
-    Rigidbody2D enemyRigidBody;
-    // Start is called before the first frame update
-    AIPath aiPath;
-    // Start is called before the first frame update
+  Rigidbody2D enemyRigidBody;
+  // Start is called before the first frame update
+  AIPath aiPath;
+  // Start is called before the first frame update
 
-    void Start()
+  void Start()
+  {
+    enemyAnimator = GetComponent<Animator>();
+    enemyRigidBody = GetComponent<Rigidbody2D>();
+    spriteRenderer = GetComponent<SpriteRenderer>();
+    aiPath = GetComponent<AIPath>();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    DumbMovement();
+    MirrorAnimation();
+    StartAttack();
+    GravityDrag();
+  }
+
+  private void GravityDrag()
+  {
+    if (CenterOfGravity != null && aiPath == null)
     {
-        enemyAnimator = GetComponent<Animator>();
-        enemyRigidBody = GetComponent<Rigidbody2D>();
-        aiPath = GetComponent<AIPath>();
-        InvokeRepeating("ChangeDirection", 0f, repeat);
+      enemyRigidBody.AddForce((CenterOfGravity.transform.position - transform.position) * GravityForce);
+      Vector3 dif = CenterOfGravity.transform.position - transform.position;
+      float RotationZ = Mathf.Atan2(dif.y, dif.x) * Mathf.Rad2Deg;
+      transform.rotation = Quaternion.Euler(0.0F, 0.0F, RotationZ + 90);
+    }
+  }
+
+  /*
+        Bot will start attack when bot and player is in a fixed distance
+        For Fog of War, add sfx when bot start moving    -> Hieu heo lam cai nay nha :D
+    */
+  void StartAttack()
+  {
+
+    if (aiPath != null)
+    {
+      if (aiPath.desiredVelocity.x <= 0.01f)
+      {
+        transform.eulerAngles = new Vector3(0, 180, 0);
+      }
+      else
+      {
+        transform.eulerAngles = new Vector3(0, 0, 0);
+      }
+
+      if (aiPath.remainingDistance <= chaseDistance)
+      {
+        aiPath.canMove = true;
+        // enemyRigidBody.simulated = true;
+      }
+      else
+      {
+        aiPath.canMove = false;
+        // enemyRigidBody.simulated = false;
+      }
     }
 
-    // Update is called once per frame
-    void Update()
+  }
+
+  private float CalculateAngularSpeedLimitation()
+  {
+
+    if (CenterOfGravity != null)
     {
-        DumbMovement();
-        StartAttack();
-        GravityDrag();
-    }
+      float speedLimitation;
+      float distance;
 
-    private void GravityDrag()
-    {
-        if (CenterOfGravity != null && aiPath == null)
-        {
-            enemyRigidBody.AddForce((CenterOfGravity.transform.position - transform.position) * GravityForce);
-            Vector3 dif = CenterOfGravity.transform.position - transform.position;
-            float RotationZ = Mathf.Atan2(dif.y, dif.x) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Euler(0.0F, 0.0F, RotationZ + 90);
-        }
-    }
+      distance = Vector3.Distance(transform.position, CenterOfGravity.transform.position);
+      distance = distance / 10;
 
-    /*
-          Bot will start attack when bot and player is in a fixed distance
-          For Fog of War, add sfx when bot start moving    -> Hieu heo lam cai nay nha :D
-      */
-    void StartAttack()
-    {
+      speedLimitation = Mathf.Lerp(0.5F, 2F, distance);
+      speedLimitation = speedLimitation / 5;
 
-        if (aiPath != null)
-        {
-            if (aiPath.desiredVelocity.x <= 0.01f)
-            {
-                transform.eulerAngles = new Vector3(0, 180, 0);
-            }
-            else
-            {
-                transform.eulerAngles = new Vector3(0, 0, 0);
-            }
-
-            if (aiPath.remainingDistance <= chaseDistance)
-            {
-                aiPath.canMove = true;
-                // enemyRigidBody.simulated = true;
-            }
-            else
-            {
-                aiPath.canMove = false;
-                // enemyRigidBody.simulated = false;
-            }
-        }
+      return speedLimitation;
 
     }
+    else { return 1; } //If the player don't have gravity center, no speed limitation is set.
+  }
 
-    private float CalculateAngularSpeedLimitation()
+  /*
+        Dumb movement left and right when player not in range
+    */
+  void DumbMovement()
+  {
+    if (aiPath == null)
     {
+      Vector2 localvelocity;
+      localvelocity = transform.InverseTransformDirection(enemyRigidBody.velocity);
+      localvelocity.x = direction * Time.deltaTime * speed * 100 * CalculateAngularSpeedLimitation();
+      enemyRigidBody.velocity = transform.TransformDirection(localvelocity);
+    }
+  }
 
-        if (CenterOfGravity != null)
-        {
-            float speedLimitation;
-            float distance;
 
-            distance = Vector3.Distance(transform.position, CenterOfGravity.transform.position);
-            distance = distance / 10;
-
-            speedLimitation = Mathf.Lerp(0.5F, 2F, distance);
-            speedLimitation = speedLimitation / 5;
-
-            return speedLimitation;
-
-        }
-        else { return 1; } //If the player don't have gravity center, no speed limitation is set.
+  private void OnTriggerEnter2D(Collider2D other)
+  {
+    if (other.gameObject.CompareTag("turn"))
+    {
+      Debug.Log("Turn");
+      direction *= -1;
     }
 
-    /*
-          Dumb movement left and right when player not in range
-      */
-    void DumbMovement()
+  }
+
+  private void MirrorAnimation()
+  {
+    Vector2 localVelocity = transform.InverseTransformDirection(enemyRigidBody.velocity);
+
+    if (localVelocity.x > 0.5F)
     {
-        if (aiPath == null)
-        {
-            Vector2 localvelocity;
-            localvelocity = transform.InverseTransformDirection(enemyRigidBody.velocity);
-            localvelocity.x = direction * Time.deltaTime * speed * 100 * CalculateAngularSpeedLimitation();
-            enemyRigidBody.velocity = transform.TransformDirection(localvelocity);
-        }
+      if (spriteRenderer.flipX == false)
+      {
+        spriteRenderer.flipX = true;
+      }
     }
 
-    void ChangeDirection()
+    else if (localVelocity.x < -0.5)
     {
-        if (moveRight == true)
-        {
-            moveRight = false;
-        }
-        else
-        {
-            moveRight = true;
-        }
+      if (spriteRenderer.flipX == true)
+      {
+        spriteRenderer.flipX = false;
+      }
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.gameObject.CompareTag("turn"))
-        {
-            Debug.Log("Turn");
-            direction *= -1;
-        }
-
-    }
+  }
 }
--- Assets/Script/secondHandsMovement.cs
+++ Assets/Script/secondHandsMovement.cs
@@ -6,21 +6,29 @@ using UnityEngine;
 public class secondHandsMovement : MonoBehaviour
 {
 
-    float rotationMultiplier = 6.0f;
-    public float timeRemaining = 60;
+  float rotationMultiplier = 6.0f;
+  public float timeRemaining = 60;
 
-    void Update()
+  void Update()
+  {
+    if (timeRemaining > 0)
     {
-        if (timeRemaining > 0)
-        {
-            timeRemaining -= Time.deltaTime;
-            transform.Rotate(Vector3.forward, Time.deltaTime * rotationMultiplier * -1f);
-        }
-        else
-        {
-            timeRemaining = 0;
-            Debug.Log("Time's up!");
-        }
-        
+      timeRemaining -= Time.deltaTime;
+      transform.Rotate(Vector3.forward, Time.deltaTime * rotationMultiplier * -1f);
     }
+    else
+    {
+      timeRemaining = 0;
+      Debug.Log("Time's up!");
+    }
+
+  }
+
+  private void OnCollisionEnter2D(Collision2D other)
+  {
+    if (other.collider.tag == "DeathTrap")
+    {
+      Physics2D.IgnoreCollision(GetComponent<Collider2D>(), other.collider);
+    }
+  }
 }
